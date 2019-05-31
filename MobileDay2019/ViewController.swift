9446ca4ea76ec1d4e9a7d472858b95c37be97b05
Trung Phan <trungpv@dwarvesv.com>
2019-05-31 12:22:00 +0700

    Init project

---
+++ MobileDay2019/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MobileDay2019
+//
+//  Created by Trung Phan on 5/28/19.
+//  Copyright © 2019 Dwarvesf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
